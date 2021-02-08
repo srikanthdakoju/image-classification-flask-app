b7b1ded81e007c836a42eb3dec905edcfa0d45a2
srikanth dakoju <srikanthdakoju@gmail.com>
2021-02-09 00:29:22 +0530

    change in model

--- models.py
+++ models.py
@@ -11,44 +11,38 @@ import pickle
 
 
 
-class rgb2gray_transform(BaseEstimator,TransformerMixin):
-    import skimage.color
-    def __init__(self):
-        pass
+# class rgb2gray_transform(BaseEstimator,TransformerMixin):
+#     import skimage.color
+#     def __init__(self):
+#         pass
     
-    def fit(self,X,y=None):
-        return self
+#     def fit(self,X,y=None):
+#         return self
     
-    def transform(self,X,y=None):
-        return np.array([skimage.color.rgb2gray(x) for x in X])
+#     def transform(self,X,y=None):
+#         return np.array([skimage.color.rgb2gray(x) for x in X])
 
-class hogtransformer(BaseEstimator,TransformerMixin):
-    import skimage.feature
-    def __init__(self,orientations=9,pixels_per_cell=(8, 8),cells_per_block=(3, 3),):
-        self.orientations = orientations
-        self.pixels_per_cell = pixels_per_cell
-        self.cells_per_block = cells_per_block
+# class hogtransformer(BaseEstimator,TransformerMixin):
+#     import skimage.feature
+#     def __init__(self,orientations=9,pixels_per_cell=(8, 8),cells_per_block=(3, 3),):
+#         self.orientations = orientations
+#         self.pixels_per_cell = pixels_per_cell
+#         self.cells_per_block = cells_per_block
         
         
-    def fit(self,X,y=None):
-        return self
+#     def fit(self,X,y=None):
+#         return self
     
-    def transform(self,X,y=None):
-        def local_hog(img):
-            hog_features= skimage.feature.hog(img,orientations=self.orientations,
-                                pixels_per_cell=self.pixels_per_cell,
-                                cells_per_block=self.cells_per_block)
+#     def transform(self,X,y=None):
+#         def local_hog(img):
+#             hog_features= skimage.feature.hog(img,orientations=self.orientations,
+#                                 pixels_per_cell=self.pixels_per_cell,
+#                                 cells_per_block=self.cells_per_block)
             
-            return hog_features
+#             return hog_features
         
-        hfeatures = np.array([local_hog(x) for x in X])
-        return hfeatures
-
-
-
-
-
-
+#         hfeatures = np.array([local_hog(x) for x in X])
+#         return hfeatures
 
 
 
